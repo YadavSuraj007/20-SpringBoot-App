92dfd1abfdcf6b28273a0e5c412b5edc18b44bbe
YadavSuraj007 <surajyadav063@gmail.con>
2022-12-07 23:36:16 +0530

    project initial commit

---
+++ src/main/java/com/nareshit/helloworld/HelloWorldWithSiApplication.java
@@ -0,0 +1,13 @@
+package com.nareshit.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWithSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWithSiApplication.class, args);
+	}
+
+}
